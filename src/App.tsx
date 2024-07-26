068eaa7ebdbfebce7038cd27d955a9326d4e6ad5
PeaceNaza <peacedonatus38@gmail.com>
2024-07-27 00:09:13 +0100

    feat: add todo list feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import './App.css'
+import InputFeild from './components/InputFeild'
+import { useState } from 'react'
+import { Todo } from "./model"
+import TodoList from './components/TodoList'
 
-const App = () => {
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("") 
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+   e.preventDefault();
+   if (todo) {
+    setTodos([...todos, {id: Date.now(), todo, isDone: false}]);
+    setTodo("");
+   }
+  }
+
+  console.log(todos)
+
   return (
-    <h1 className='text-center'>
-      hellow world
-    </h1>
+    <>
+      <div className='w-screen h-screen flex flex-col items-center bg-[#2f74c0]'>
+        <span className='uppercase md:text-[40px] text-white md:mx-0 md:my-8 z-[1] text-center xs:my-4 xs:text-[35px]'>Taskify</span>
+        <InputFeild handleAdd={handleAdd}  todo={todo} setTodo={setTodo} />
+        <TodoList todos={todos} setTodos={setTodos} />
+       
+      </div>
+    </>
+  
   )
 }
 
---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,85 @@
+import React, { useState, useRef, useEffect }  from 'react'
+import { Todo } from "../model"
+import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
+import { MdDone } from "react-icons/md";
+
+
+
+
+type Props = {
+  todo: Todo,
+  todos: Todo[],
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const SingleTodo = ({todo, todos, setTodos}:Props) => {
+
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+
+    setTodos(
+      todos.map((todo) => (
+        todo.id === id ? {...todo, todo: editTodo} : todo
+    ))
+  );
+  setEdit(false);
+  }
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+   }, [edit] )
+
+
+  const handleDone = (id: number) => { 
+    setTodos(
+      todos.map((todo) => 
+        todo.id === id ? {...todo, isDone: !todo.isDone} : todo
+    ));
+    }
+
+    const handleDelete = (id: number) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    }
+
+  return (
+    <form className='image flex md:w-[29.5%] rounded-md p-5 mt-4 xs:w-full' onSubmit={(e) => handleEdit(e, todo.id)}>
+
+     {
+        edit ? (
+          <input ref={inputRef} placeholder='Edit todo' type='text' value={editTodo} onChange={(e) => setEditTodo(e.target.value)} className='flex-1 p-[5px] border-0 text-[20px] focus:outline-none' />
+        ) : (
+          
+          todo.isDone? (
+            <s className='flex-1 p-[5px] border-0 text-[20px] focus:outline-none'>{todo.todo}</s>
+    
+           ) : (
+            <span className='flex-1 p-[5px] border-0 text-[20px] focus:outline-none'>{todo.todo}</span>
+    
+           )
+        )
+     }
+
+    
+     
+      <div className='flex flex-row'>
+        <span className='ml-[10px] text-[25px] cursor-pointer' onClick={() => {
+         if (!edit && !todo.isDone) {
+           setEdit(!edit);
+         }  
+
+        }}>
+          <AiFillEdit />
+          </span>
+        <span className='ml-[10px] text-[25px] cursor-pointer' onClick={() => handleDelete(todo.id)}><AiFillDelete /></span>
+        <span className='ml-[10px] text-[25px] cursor-pointer' onClick={() => handleDone(todo.id)}><MdDone /></span>
+      </div>
+    </form>
+  )
+}
+
+export default SingleTodo
